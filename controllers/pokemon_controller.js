8e023ede89aacc52084169da493d50c6ca85dcd6
guisantosalves <workguisantos@gmail.com>
2022-05-11 12:18:46 -0500

    consumindo da api pokeapi

--- controllers/pokemon_controller.js
+++ controllers/pokemon_controller.js
@@ -1,15 +1,53 @@
-
-const axios = require('axios').default;
-
+const axios = require("axios").default;
 
 module.exports.getDataOfPokemon = (req, res, next) => {
-
+  try {
     //pegando da api do pokemon
-    axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=150')
-    .then((response)=>{
-        res.send(response.data)
-    }).catch((error)=>{
-        console.log(error)
-    })
+    //https://pokeapi.co/api/v2/ability/
+    //https://pokeapi.co/api/v2/pokemon?offset=0&limit=150
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (err) {
+    res.status(400);
+    res.send(err);
+  }
+};
 
-}
+module.exports.getDataOfhabilities = (req, res, next) => {
+  try {
+    axios
+      .get("https://pokeapi.co/api/v2/ability?offset=0&limit=30")
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (err) {
+    res.status(400);
+    res.send(err);
+  }
+};
+
+module.exports.getDataOfHabitats = (req, res) => {
+  //https://pokeapi.co/api/v2/pokemon-habitat/
+  try {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon-habitat/")
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (err) {
+    res.status(400);
+    res.send(err);
+  }
+};
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var pokemon_controller = require('../controllers/pokemon_controller');
 
 router.get('/poke', pokemon_controller.getDataOfPokemon);
-
+router.get('/pokehabilities', pokemon_controller.getDataOfhabilities);
+router.get('/pokemonhabitats', pokemon_controller.getDataOfHabitats)
 module.exports = router;
